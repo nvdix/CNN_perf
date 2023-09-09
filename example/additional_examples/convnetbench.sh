65fce9c490436f2d73e2312bf8ecfe1a59bba07c
Aleksey <47855450+lehador1@users.noreply.github.com>
2023-09-10 00:14:12 +0300

    Add files via upload

---
+++ example/additional_examples/convnetbench.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Проверка наличия аргументов командной строки
+if [ "$#" -eq 0 ]; then
+    echo "Ошибка: необходимо указать хотя бы один аргумент."
+    exit 1
+fi
+
+# Инициализация переменной для хранения общего числа операций в секунду
+total_time_inv=0.0
+
+# Функция для вызова утилиты и добавления результатов к общему числу
+calculate_ops() {
+    local layer_results=$(./convbench -q "$@")
+    echo "Слои: $@"
+    IFS=$'\n'
+    for layer_ops in $layer_results; do
+        echo "    $layer_ops операций в секунду"
+        layer_time_inv=$(echo "1 / $layer_ops" | bc -l)
+        total_time_inv=$(echo "$total_time_inv + $layer_time_inv" | bc -l)
+    done
+}
+
+# Вызов утилиты с параметрами из аргументов командной строки
+calculate_ops "$@"
+
+# Вывод общего числа операций в секунду
+total_ops=$(echo "1 / $total_time_inv" | bc -l)
+echo "Операций в секунду по всем слоям: $total_ops"
